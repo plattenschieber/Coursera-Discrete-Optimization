a2f8e3afb46e5131c01c2a6789a31e9b12a44282
Jeronim Morina <plattenschieber@gmail.com>
2015-03-07 14:34:01 +0100

    dynamic programming is optimal

--- knapsack/Solver.java
+++ knapsack/Solver.java
@@ -101,7 +101,7 @@ public class Solver {
         }
         
         // prepare the solution in the specified output format
-        System.out.println(value+" 0");
+        System.out.println(value+" 1");
         for(int i=0; i < items; i++){
             System.out.print(taken[i]+" ");
         }
